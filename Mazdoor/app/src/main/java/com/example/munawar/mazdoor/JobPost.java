ef0aae87fad0aaccf30eb0ab6f729a0f21b416b3
Munawar Bhutto <bhuttomunawar@gmail.com>
2018-10-13 18:34:29 +0500

    Sign Up, Main screen, Job post menu

--- Mazdoor/app/src/main/java/com/example/munawar/mazdoor/JobPost.java
+++ Mazdoor/app/src/main/java/com/example/munawar/mazdoor/JobPost.java
@@ -1,17 +1,13 @@
 package com.example.munawar.mazdoor;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class JobPost extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Intent i = new Intent(Login.this, Main2Activity.class);
-        startActivity(i);
-        finish();
+        setContentView(R.layout.activity_job_post);
     }
 }
